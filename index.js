55fc0026ae95287d2f3014127f9b1f8cdb8b20ce
cpach96 <cpach96@gmail.com>
2021-03-23 09:16:54 -0400

    commit tree d50556841b2597e514e12c665ee619cd5e0c4d02

--- index.js
+++ index.js
@@ -12,5 +12,5 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(tutorial => tutorial.charAt(0).toUpperCase() + tutorial.substr(1).toLowerCase());
 }
